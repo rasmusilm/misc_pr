8b031baab1a2eaaf1d62492cbff182484df5b521
railmj <railmj@ttu.ee>
2022-02-02 20:07:32 +0200

    Reworked, added new introduction

--- js/calculateSemester.js
+++ js/calculateSemester.js
@@ -1,11 +1,16 @@
-const toChange = document.getElementById("is-finished");
+const toChange = document.getElementById("finished");
 
 let month = new Date().getMonth();
 let year = new Date().getFullYear();
-if (month >= 5 && year === 2022 && month < 9) {
-    toChange.innerText = "lõpetatud";
-    document.getElementById("js-image").innerHTML = '<a title="Ramaksoud2000 via Chris Williams, Public domain, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:JavaScript-logo.png"><img class="lang-logo" alt="JavaScript-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/512px-JavaScript-logo.png"></a>'
-} else if (year === 2022 && month >= 9) {
-    document.getElementById('semester').innerText = "5";
+month = 9;
+if (month >= 5 && year === 2022) {
+    toChange.innerText = "4";
+    document.getElementById("js-image").innerHTML = '<a title="Ramaksoud2000 via Chris Williams, Public domain, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:JavaScript-logo.png"><img class="lang-logo" alt="JavaScript-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/512px-JavaScript-logo.png"></a>';
+    if (month >= 5 && month < 9) {
+        document.getElementById("half-way").innerHTML = "";
+    } else if (month >= 9) {
+        document.getElementById('current-semester').innerText = "5";
+    }
 }
+
 console.log(month);
